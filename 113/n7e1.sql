d0e326338e05e9ee647900359c357ec4a6a139da
Daniel Hoffmann <oralordos@gmail.com>
2019-12-10 15:16:05 -0800

    Ninja 7 Ex 1

---
+++ 113/n7e1.sql
@@ -0,0 +1,3 @@
+SELECT pfirstname || ' ' || plastname || ' ' || ppoints FROM people;
+-- SELECT pfirstname || ' ' || plastname || ' ' || CAST(ppoints AS TEXT) FROM people;
+-- SELECT pfirstname || ' ' || plastname || ' ' || ppoints::TEXT FROM people;
