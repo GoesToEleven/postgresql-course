45341e91c74adb676c7ac86a1a53cdb9ede5130d
Todd McLeod <tuddleymc@gmail.com>
2019-12-13 16:23:25 -0800

    setup

---
+++ 143-movie-database/all-sql-commands.sql
@@ -0,0 +1,84 @@
+-- CREATE DATABASE movies;
+
+DROP TABLE IF EXISTS customers CASCADE;
+DROP TABLE IF EXISTS movies CASCADE;
+DROP TABLE IF EXISTS rentals CASCADE;
+DROP TABLE IF EXISTS employees CASCADE;
+DROP TABLE IF EXISTS department CASCADE;
+
+CREATE TABLE customers(
+    cid SERIAL PRIMARY KEY, 
+    cname VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE movies(
+    mid SERIAL PRIMARY KEY, 
+    mname TEXT NOT NULL 
+);
+
+CREATE TABLE rentals(
+    rid SERIAL PRIMARY KEY, 
+    cid INT REFERENCES customers NOT NULL, 
+    mid INT REFERENCES movies NOT NULL
+);
+
+CREATE TABLE employees(
+   eID SERIAL PRIMARY KEY,
+   eName VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE department(
+   dID SERIAL PRIMARY KEY,
+   dept VARCHAR(50), 
+   eID INT REFERENCES employees
+);
+-- NOTE dept and eID can be NULL to illustrate joins
+
+
+INSERT INTO customers (cname) VALUES 
+('James'),
+('Moneypenny'),
+('Q'),
+('M'),
+('Ian'),
+('Fleming');
+
+INSERT INTO movies (mname) VALUES 
+('Ex Machina'),
+('A Man Called Ove'),
+('The Lives of Others'),
+('Intouchables'),
+('Limitless'),
+('Kumare');
+
+INSERT INTO rentals (cid, mid) VALUES 
+(2,3), 
+(2,4), 
+(4,1), 
+(3,6), 
+(1,4), 
+(5,2), 
+(6,1), 
+(5,5), 
+(4,3), 
+(3,1), 
+(2,2), 
+(1,3), 
+(1,4), 
+(2,6), 
+(4,5);
+
+INSERT INTO employees (ename) VALUES 
+('Stacey'),
+('Shelley'),
+('Samantha'),
+('Shannon'),
+('Sherry'),
+('Sylvia');
+
+INSERT INTO department (dept, eid) VALUES
+('Sales', 1),
+('Sales', 2),
+('Sales', 3),
+('Manager', 4),
+('Owner', null);
---
+++ 143-movie-database/testing-files.sql/01-examples.sql
@@ -0,0 +1,32 @@
+-- INNER JOIN
+-- SELECT employees.ename, department.dept FROM employees JOIN department ON employees.eid = department.eid;
+-- SELECT employees.eid FROM employees INTERSECT SELECT department.eid FROM department;
+
+-- LEFT JOIN
+-- SELECT employees.ename, department.dept FROM employees LEFT JOIN department ON employees.eid = department.eid;
+
+-- RIGHT JOIN
+-- SELECT employees.ename, department.dept FROM employees RIGHT JOIN department ON employees.eid = department.eid;
+
+-- FULL JOIN
+-- SELECT employees.ename, department.dept FROM employees FULL JOIN department ON employees.eid = department.eid;
+
+-- EXCEPT
+-- SELECT employees.eid FROM employees EXCEPT SELECT department.eid FROM department;
+-- SELECT department.eid FROM department EXCEPT SELECT employees.eid FROM employees;
+
+-- UNION
+SELECT cname FROM customers UNION SELECT mname FROM movies;
+-- SELECT cname FROM customers UNION SELECT mname FROM movies ORDER BY cname;
+-- SELECT cname AS "RESULTS" FROM customers UNION SELECT mname FROM movies ORDER BY "RESULTS";
+
+-- some of the above can also be written as ...
+-- SELECT employees.ename, department.dept FROM employees INNER JOIN department ON employees.eid = department.eid;
+-- SELECT employees.ename, department.dept FROM employees LEFT OUTER JOIN department ON employees.eid = department.eid;
+-- SELECT employees.ename, department.dept FROM employees RIGHT OUTER JOIN department ON employees.eid = department.eid;
+-- SELECT employees.ename, department.dept FROM employees FULL OUTER JOIN department ON employees.eid = department.eid;
+
+
+
+-- CROSS JOIN
+-- SELECT * FROM employees CROSS JOIN department;
