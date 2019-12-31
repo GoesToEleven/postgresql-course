b40e8c38c7548e0bb042b290467b1672d8d1b937
Todd McLeod <tuddleymc@gmail.com>
2019-12-31 15:31:10 -0800

    setup

---
+++ 999 PREP GROUPING HAVING/setup.sql
@@ -0,0 +1,30 @@
+-- SELECT people.pcity, COUNT(*) AS "people per city" from people GROUP BY people.pcity;
+-- SELECT people.pcity, COUNT(*) AS "people per city" from people GROUP BY people.pcity ORDER BY "people per city" DESC;
+
+-- SELECT people.pstate, COUNT(*) AS "people per city" from people GROUP BY people.pstate;
+-- SELECT people.pstate, COUNT(*) AS "people per city" from people GROUP BY people.pstate ORDER BY "people per city" DESC;
+
+-- MORE THAN TWO VALUES
+-- anything that is not an AGGREGATE function must be in the GROUP BY
+-- SELECT people.pcity, people.pstate, COUNT(*) AS "people per city" from people GROUP BY people.pcity, people.pstate;
+-- SELECT people.pcity, people.pstate, COUNT(*) AS "people per city" from people GROUP BY people.pcity, people.pstate ORDER BY "people per city" DESC, people.pcity ASC LIMIT 10;
+-- SELECT people.pcity, people.pstate, COUNT(*) AS "people per city" from people GROUP BY people.pstate, people.pcity ORDER BY "people per city" DESC, people.pcity ASC LIMIT 10;
+
+-- ORDERING
+-- WHERE and HAVING
+-- SELECT people.pcity, people.pstate, COUNT(*) AS "people per city" from people 
+-- GROUP BY people.pcity, people.pstate 
+-- HAVING COUNT(*) >= 2
+-- ORDER BY "people per city" DESC, people.pcity ASC;
+
+
+-- SELECT people.pcity, people.pstate, COUNT(*) AS "people per city" from people 
+-- WHERE pcity LIKE 'A%'
+-- GROUP BY people.pcity, people.pstate 
+-- HAVING COUNT(*) >= 2
+-- ORDER BY "people per city" DESC, people.pcity ASC;
+
+-- HANDS ON EXERCISES
+-- SELECT * FROM people;
+
+SELECT people.pfirstname, people.plastname, COUNT()
---
+++ 999 PREP GROUPING HAVING/setupmovies.sql
@@ -0,0 +1,23 @@
+-- HANDS ON EXERCISES
+SELECT customers.cname, COUNT(*) AS "number of rentals" 
+FROM customers 
+LEFT JOIN rentals ON (customers.cid=rentals.cid)
+LEFT JOIN movies ON (rentals.mid=movies.mid)
+GROUP BY customers.cname;
+
+SELECT customers.cname, COUNT(*) AS "number of rentals" 
+FROM customers 
+LEFT JOIN rentals ON (customers.cid=rentals.cid)
+LEFT JOIN movies ON (rentals.mid=movies.mid)
+GROUP BY customers.cname
+ORDER BY COUNT(*) DESC;
+
+SELECT customers.cname, COUNT(*) AS "number of rentals" 
+FROM customers 
+LEFT JOIN rentals ON (customers.cid=rentals.cid)
+LEFT JOIN movies ON (rentals.mid=movies.mid)
+WHERE customers.cname LIKE '%e%'
+GROUP BY customers.cname
+ORDER BY COUNT(*) DESC;
+
+select * from customers;
