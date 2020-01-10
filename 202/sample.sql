08c1a60dedb715b397dfe287482efafa290b292a
Daniel Hoffmann <oralordos@gmail.com>
2020-01-10 14:23:04 -0800

    Fix example

--- 202/sample.sql
+++ 202/sample.sql
@@ -4,11 +4,11 @@ SELECT * FROM customers;
 SELECT * FROM rentals;
 SELECT * FROM movies;
 
-DELETE FROM customers 
+DELETE FROM customers
 USING rentals, movies
 WHERE customers.cid = rentals.cid
     AND rentals.mid = movies.mid
-    AND movies.mid = 3
+    AND movies.mname = 'The Lives of Others'
 RETURNING *;
 
 SELECT * FROM customers;
